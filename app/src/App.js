8eb829a1f6e80b6ba0cda3e7bd2053f94d3a2a11
Jozark <jorekplus@gmail.com>
2022-10-03 17:16:50 +0200

    create scaffold

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
+import { Connection, PublicKey, Keypair } from "@solana/web3.js"
+import { Program, Provider, web3 } from "@project-serum/anchor"
+import idl from "./idl.json"
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
+import {
+  useWallet,
+  WalletProvider,
+  ConnectionProvider,
+} from "@solana/wallet-adapter-react"
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui"
+require("@solana/wallet-adapter-react-ui/styles.css")
+
+const wallets = [
+  // instantiate other wallet adapters if needed
+  new PhantomWalletAdapter(),
+]
+const { Keypair, SystemProgram } = web3
+const switchAccount = Keypair.generate()
+const programId = new PublicKey(idl.metadata.address) // because it is a keypair
+const opts = {
+  preflightCommitment: "Processed", // wait for a processed confirmation
+}
 
 function App() {
+  const [value, setValue] = useState(null)
+  const wallet = useWallet()
+  async function getProvider() {
+    // collect all necessary information about the user
+    const network = "http://127.0.0.1:8899"
+    const connection = new Connection(network, opts.preflightCommitment)
+    const provider = new Provider(connection, wallet, opts.preflightCommitment)
+    return provider
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +54,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
